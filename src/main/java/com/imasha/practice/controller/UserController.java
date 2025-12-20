05ce6856f84a8fe3df518da2a67e432e006408d0
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-12-20 22:27:33 +0530

    setup getuser api

--- src/main/java/com/imasha/practice/controller/UserController.java
+++ src/main/java/com/imasha/practice/controller/UserController.java
@@ -1,9 +1,6 @@
 package com.imasha.practice.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController // used to create REST APIs that return JSON / text data
 @RequestMapping(value = "api/v1/user") // base URL mapping
@@ -13,6 +10,19 @@ public class UserController {
     @GetMapping("/getuser")
     public String getUser() {
         return "imasha";
+    }
+    @PostMapping("/saveuser")
+    public String saveUser() {
+        return "saved";
+    }
+    @PutMapping("/updateuser")
+    public String updateUser(){
+        return "edit";
+    }
+    @DeleteMapping("/deleteuser")
+
+    public String deleteUser(){
+        return("delete");
     }
 }
 
