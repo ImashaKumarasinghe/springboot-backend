e70a6e82f310d0151ea8b6544b44ecbda7e0e9d1
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-12-21 16:21:03 +0530

    add modelmapper into main method

--- src/main/java/com/imasha/practice/PracticeApplication.java
+++ src/main/java/com/imasha/practice/PracticeApplication.java
@@ -1,7 +1,9 @@
 package com.imasha.practice;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PracticeApplication {
@@ -9,5 +11,12 @@ public class PracticeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeApplication.class, args);
 	}
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+
+
 
 }
