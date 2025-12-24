c771876ce22ad52baa2f151a839e77bc5ab232d8
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-12-24 12:07:07 +0530

    include native query=true for identify it as native quey type as well as execute query and data pass into User entity

--- src/main/java/com/imasha/practice/repo/UserRepo.java
+++ src/main/java/com/imasha/practice/repo/UserRepo.java
@@ -2,12 +2,13 @@ package com.imasha.practice.repo;
 
 import com.imasha.practice.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.NativeQuery;
 import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepo extends JpaRepository<User,Integer> {
 
     //?1 is first parameter ?1 refers to userId 1 means position number?2 would mean the second parameter, and so on
-    @Query(value = "SELECT*FROM USER WHERE=?1")
+    @Query(value = "SELECT*FROM USER WHERE=?1", nativeQuery = true)
     User getUserByUserId(String userId);
 
 }
