d9d312dbcd447b5e456856518fb3cd48c15bc3ca
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-12-24 11:16:14 +0530

    query for get users by id

--- src/main/java/com/imasha/practice/repo/UserRepo.java
+++ src/main/java/com/imasha/practice/repo/UserRepo.java
@@ -2,7 +2,11 @@ package com.imasha.practice.repo;
 
 import com.imasha.practice.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepo extends JpaRepository<User,Integer> {
+
+    @Query(value = "SELECT*FROM USER WHERE=?1")
+    User getUserById(String userId);
 
 }
